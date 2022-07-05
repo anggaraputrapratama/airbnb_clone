1bab7eefee405e4519cbc80ae93ef6ee7459e88b
anggaraputrapratama <anggaraputraa.pratama@gmail.com>
2022-07-05 08:32:21 +0700

    add search_input with fade_app_bar and grid place

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:airbnb_clone/widgets/city_widgets.dart';
+import 'package:airbnb_clone/widgets/fade_app_bar.dart';
 import 'package:airbnb_clone/widgets/header.dart';
+import 'package:airbnb_clone/widgets/live_anywhere_widgets.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/hero_banner.dart';
@@ -12,14 +14,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late ScrollController _scrollController;
+  double _scrollControllerOffset = 0.0;
+
+  void _scrollListener() {
+    setState(() {
+      _scrollControllerOffset = _scrollController.offset;
+    });
+  }
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    _scrollController.addListener(_scrollListener);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: const [
+        children: [
           CustomScrollView(
-            slivers: [HeroBanner(), Header('Explore Nearby'), CitiesGrid()],
+            controller: _scrollController,
+            slivers: const [
+              HeroBanner(),
+              Header('Explore Nearby'),
+              CitiesGrid(),
+              Header("Live Anywhere"),
+              LiveAnywhereList()
+            ],
           ),
+          FadeAppBar(scrollOffset: _scrollControllerOffset)
         ],
       ),
     );
---
+++ lib/widgets/fade_app_bar.dart
@@ -0,0 +1,21 @@
+import 'package:airbnb_clone/widgets/search_input.dart';
+import 'package:flutter/material.dart';
+
+class FadeAppBar extends StatelessWidget {
+  final double scrollOffset;
+  const FadeAppBar({required this.scrollOffset, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      top: false,
+      child: Container(
+        height: 100,
+        padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 24.0),
+        color: Colors.white
+            .withOpacity((scrollOffset / 350).clamp(0, 1).toDouble()),
+        child: const SafeArea(child: SearchInput()),
+      ),
+    );
+  }
+}
